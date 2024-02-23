65c78ffa83601ab989d602267e3e9cf92f95992a
AlvaroCareli <122116260+alvarosamp@users.noreply.github.com>
2024-02-22 22:28:33 -0300

    Aula1

---
+++ main.java
@@ -0,0 +1,13 @@
+public class main {
+    public static void main(String[] args){
+        int lanche1 = 10;
+        int lanche2 = 4;
+        int lanche3 = 2;
+
+        int total = lanche1 + lanche2 + lanche3;
+        float media = total/3.0f;
+
+        System.out.println("Total de lanches: "+total);
+        System.out.println("Media de lanches: "+media);
+    }
+}
