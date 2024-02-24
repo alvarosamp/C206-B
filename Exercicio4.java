f153bd732b07d1894490d2636939f9f52e3d15bc
AlvaroCareli <122116260+alvarosamp@users.noreply.github.com>
2024-02-24 18:47:36 -0300

    Exercicio 4(24/02)

---
+++ Exercicio4.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Exercicio4 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int alunos;
+        System.out.print("Quantos alunos tem na sala?: ");
+        alunos = entrada.nextInt();
+        switch(alunos){
+            case 10 :
+                System.out.println("Sua sala é a : l-16");
+                break;
+
+            case 20:
+                System.out.println("Sua sala é a : l-16");
+                break;
+            case 30:
+                System.out.println("Sua sala é a : l-22");
+                break;
+
+            default:
+                System.out.println("Digite um valor correto");
+        }
+        entrada.close();
+    }
+}
